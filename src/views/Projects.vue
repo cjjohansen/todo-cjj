416c9c87149af026b9ab1d667bc353e7d13e3d19
cjjohansen <chjo@danskkabeltv.dk>
2020-01-20 19:31:55 +0100

    chip color and float right works chapter 16

---
+++ src/views/Projects.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="projects">
+   <h1 class="subheading grey--text">Project</h1>
+       <v-container my-5>
+        Content
+        </v-container>   
+      
+      </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'projects',
+  components: {
+   
+  }
+}
+</script>
---
+++ src/views/Team.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="team">
+        <h1 class="subheading grey--text">Team</h1>
+        <v-container my-5>
+        Content
+        </v-container>    
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'team',
+  components: {
+   
+  }
+}
+</script>
